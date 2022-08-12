48d2c095d2204f9e3801533469c70f2ea08ff76a
Trey Woodlief <adwiii7@gmail.com>
2022-08-12 11:20:29 -0400

    Add re-run until success for basePreds.sh

--- semLidarFuzzTool/controllers/modelBasePred/basePreds.sh
+++ semLidarFuzzTool/controllers/modelBasePred/basePreds.sh
@@ -8,22 +8,26 @@ bins="/home/garrett/Documents/data/dataset/sequences/"
 stage="/home/garrett/Documents/data/tmp/dataset/sequences/00/velodyne/"
 pred="/home/garrett/Documents/data/resultsBase/"
 # model="sq3"
-model="js3c_gpu"
+# model="js3c_gpu"
 # model=dar
 
 # ------------------------------------------
 
-model="js3c_gpu"
-python modelBasePred.py -bins $bins -stage $stage -pred $pred -model $model
-model="js3c_cpu"
-python modelBasePred.py -bins $bins -stage $stage -pred $pred -model $model
+# model="js3c_gpu"
+# python modelBasePred.py -bins $bins -stage $stage -pred $pred -model $model
 
+# because the model sometimes runs out of memory, keep trying
+model="js3c_cpu"
+until python modelBasePred.py -bins $bins -stage $stage -pred $pred -model $model
+do
+  echo "Trying again"
+done
 
 # ------------------------------------------
 
 
-#model="ran"
-#python modelBasePred.py -bins $bins -stage $stage -pred $pred -model $model
+# model="ran"
+# python modelBasePred.py -bins $bins -stage $stage -pred $pred -model $model
 
 
 
